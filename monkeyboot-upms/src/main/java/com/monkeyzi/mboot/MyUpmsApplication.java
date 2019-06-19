bd3bfec60e3c83c9d369d81dd34ea5e4a97ec7da
854152531@qq.com <854152531@qq.com>
2019-06-19 21:40:58 +0800

    init project

---
+++ monkeyboot-upms/src/main/java/com/monkeyzi/mboot/MyUpmsApplication.java
@@ -0,0 +1,19 @@
+package com.monkeyzi.mboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: 高yg
+ * @date: 2019/6/19 21:30
+ * @qq:854152531@qq.com
+ * @blog http://www.monkeyzi.xin
+ * @description:
+ */
+@SpringBootApplication
+public class MyUpmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyUpmsApplication.class);
+    }
+}
